e2a8cffac25ee5ee7c842098452ddd91e2c17196
s0a07ub <shikhar.agarwal@walmart.com>
2024-08-08 20:10:50 +0530

    Committing sql Sample changes

---
+++ src/main/java/org/sample/sql/service/UserService.java
@@ -0,0 +1,25 @@
+package org.sample.sql.service.sql.service;
+
+import org.sample.sql.service.sql.entities.User;
+import org.sample.sql.service.sql.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User createUser(User user) {
+        return userRepository.saveAndFlush(user);
+    }
+
+}
