f48e0c5ad9d53258ebf07eec5843e56671119e3e
s0a07ub <shikhar.agarwal@walmart.com>
2024-08-08 19:51:37 +0530

    Committing kafka-consumer Sample changes

---
+++ src/main/java/org/examplekafka/consumer/KafkaConsumerConfig.java
@@ -0,0 +1,25 @@
+package org.kafkaconsumer.consumer;
+
+import org.kafkaconsumer.config.BaseKafkaConsumerConfig;
+import org.kafkaconsumer.config.KafkaConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+
+@EnableKafka
+@Configuration
+
+public class KafkaConsumerConfig extends BaseKafkaConsumerConfig {
+
+    @Autowired
+    KafkaConfig endgameConfig;
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+
+        return kafkaListenerContainerFactory(endgameConfig, String.class);
+    }
+
+}
